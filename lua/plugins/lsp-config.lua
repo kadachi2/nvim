26b3557e326075a083f6a5e92d299b281ab433e7
kadachi2 <kadachi1@uci.edu>
2025-01-17 23:28:43 +0900

    add markdown plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,13 +20,8 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      local on_attach = function(client)
-        require("completion").on_attach(client)
-      end
-
       lspconfig.lua_ls.setup({})
       lspconfig.rust_analyzer.setup {
-        on_attach = on_attach,
         settings = {
           ['rust-analyzer'] = {
             check = {
