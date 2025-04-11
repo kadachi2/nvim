f21d26f747ecff359a7e238b8806c9c208bc7b53
kadachi2 <kadachi1@uci.edu>
2025-04-11 19:55:05 +0900

    add hybrid line numbering with automatic toggling

--- init.lua
+++ init.lua
@@ -9,3 +9,26 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Automatic toggling between line number modes
+vim.cmd("set nu")
+
+local numbertogglegroup = vim.api.nvim_create_augroup("numbertoggle", {})
+vim.api.nvim_create_autocmd(
+    {"BufEnter", "FocusGained", "InsertLeave"},
+    {
+      pattern = '*',
+      callback = function()
+        vim.wo.relativenumber = true
+      end,
+      group = numbertogglegroup
+    })
+vim.api.nvim_create_autocmd(
+    {"BufLeave", "FocusLost", "InsertEnter"},
+    {
+      pattern = '*',
+      callback = function()
+        vim.wo.relativenumber = false
+      end,
+      group = numbertogglegroup
+    })
+
